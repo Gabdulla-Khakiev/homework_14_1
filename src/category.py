725d17b5577ef04c7a53767997077395709f2124
Gabdulla <gabdulla2001@gmail.com>
2024-10-04 23:29:53 +0300

    add new methods and property

--- src/category.py
+++ src/category.py
@@ -1,3 +1,5 @@
+from src.product import Product
+
 class Category:
     name: str
     description: str
@@ -8,7 +10,19 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.total_categories += 1
         Category.total_products += len(products) if products else 0
+
+    @property
+    def products(self):
+        product_str = ''
+        for product in self.__products:
+            product_str += f'{product.name}, {product.price}. Остаток: {product.quantity}.'
+        return product_str
+
+    @products.setter
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.total_products += 1
