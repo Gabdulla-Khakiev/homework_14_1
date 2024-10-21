a36d208985e30c24a77c10f79e0900bd11782519
Gabdulla <gabdulla2001@gmail.com>
2024-10-21 15:19:01 +0300

    small fixes in exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,5 @@
 class ZeroQuantityError(Exception):
     """Исключение для обработки добавления товаров с нулевым количеством"""
 
-    def __init__(self, message="Товар с нулевым количеством не может быть добавлен."):
-        self.message = message
-        super().__init__(self.message)
+    def __init__(self, message=None):
+        super().__init__(message)
